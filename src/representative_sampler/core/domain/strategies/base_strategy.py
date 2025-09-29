783f30dae4024a5bf3e94a0c0ccd7fb08e4edb2c
linus <lagbleze@st.ug.edu.gh>
2025-09-29 21:39:32 +0200

    feat: add SamplingStrategy

--- src/representative_sampler/core/domain/strategies/base_strategy.py
+++ src/representative_sampler/core/domain/strategies/base_strategy.py
@@ -1,8 +1,19 @@
 from abc import ABC, abstractmethod
+from representative_sampler.core.domain.entities import Sample
+from typing import List, Literal, Union
+
 
 class Sampler(ABC):
     
     @abstractmethod
     def sample(self):
+        pass
+    
+    
+class SamplingStrategy(ABC):
+    name: str
+    
+    @abstractmethod
+    def sample(self, items: List[Sample], k: int) -> List[Sample]:
         pass
     
