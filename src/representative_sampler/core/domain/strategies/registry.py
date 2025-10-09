593aad94c15e0f0a0d1facc86992bd718ae37567
linus <lagbleze@st.ug.edu.gh>
2025-10-09 19:44:11 +0200

    feat: update Registry with status

--- src/representative_sampler/core/domain/strategies/registry.py
+++ src/representative_sampler/core/domain/strategies/registry.py
@@ -5,17 +5,24 @@ class Registry:
     def __init__(self):
         self._registry = {}
         
-    def register(self, name: str, cls: type):
+    def register(self, name: str, cls: type, status:str):
         if name in self._registry:
             raise ValueError(f"{name} is already registered.")
         
-        self._registry[name] = cls
+        self._registry[name] = {name: cls, "status": status}
         
-    def get(self, name: str):
-        if name not in self._registry:
+    def get(self, name: str, status: str):
+        if name not in self._registry.keys():
             raise ValueError(f"{name} is not in registry.")
-        return self._registry[name]
+        obj = self._registry[name]
+        if status == obj["status"]:
+            return obj[name]
+        else:
+            raise ValueError(f"{name} is registered as {obj['status']}, not {status}.")
     
-    def list_available(self):
-        return set(self._registry.keys())
+    def list_available(self, status = None):
+        if not status:
+            return set(self._registry.keys())
+        else:
+            return set([name for name, obj in self._registry.items() if obj["status"] == status])
         
