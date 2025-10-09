5201ae80cb3e6e33be87ce97e4b49fcd2df95249
linus <lagbleze@st.ug.edu.gh>
2025-10-09 18:38:13 +0200

    feat: add valid_status_values to Sampler

--- src/representative_sampler/core/domain/strategies/base_strategy.py
+++ src/representative_sampler/core/domain/strategies/base_strategy.py
@@ -12,12 +12,16 @@ class Sampler(ABC):
     status: Literal["stable", "experimental"] = "experimental"
     #version: str = "0.1.0"
     required = ["sampler_name", "status"] #, "version"]
+    valid_status_values = ["stable", "experimental"]
     
     def __init_subclass__(cls):
         super().__init_subclass__()
         for attr in cls.required:
             if not hasattr(cls, attr):
                 raise NotImplementedError(f"{cls.__name__} requires definining a 'name' class attribute because it is a Subclass of Sampler.")
+        
+        if cls.status not in cls.valid_status_values:
+            raise ValueError(f"{cls.__name__} has invalid status '{cls.status}'. Valid values are {cls.valid_status_values}.")
         
         if cls.status == "stable":
             STABLE_SAMPLER_REGISTRY.register(cls.sampler_name, cls)
