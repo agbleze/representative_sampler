5740d612bc1b547b7d948be372564337c159470e
linus <lagbleze@st.ug.edu.gh>
2025-10-09 18:34:41 +0200

    feat: add automatic registering of strategy subclass

--- src/representative_sampler/core/domain/strategies/base_strategy.py
+++ src/representative_sampler/core/domain/strategies/base_strategy.py
@@ -1,13 +1,33 @@
 from abc import ABC, abstractmethod
 from representative_sampler.core.domain.entities import Sample
 from typing import List, Literal, Union
+from representative_sampler.core.domain.strategies.registry import Registry
+
+STABLE_SAMPLER_REGISTRY = Registry()
+EXPERIMENTAL_SAMPLER_REGISTRY = Registry()
 
 
 class Sampler(ABC):
+    sampler_name = "base_sampler"
+    status: Literal["stable", "experimental"] = "experimental"
+    #version: str = "0.1.0"
+    required = ["sampler_name", "status"] #, "version"]
     
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        for attr in cls.required:
+            if not hasattr(cls, attr):
+                raise NotImplementedError(f"{cls.__name__} requires definining a 'name' class attribute because it is a Subclass of Sampler.")
+        
+        if cls.status == "stable":
+            STABLE_SAMPLER_REGISTRY.register(cls.sampler_name, cls)
+        elif cls.status == "experimental":
+            EXPERIMENTAL_SAMPLER_REGISTRY.register(cls.sampler_name, cls)
+        
+        
     @abstractmethod
     def sample(self):
-        pass
+        raise NotImplementedError("Subclasses must implement the sample method.")
     
     
 class SamplingStrategy(ABC):
