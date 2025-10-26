d9781b2e2c052f7e1f47732216b1b89c5be3d0d7
linus <lagbleze@st.ug.edu.gh>
2025-10-26 23:28:36 +0100

    feat: update compute_fuzzy_cmeans_entropy

--- src/representative_sampler/core/strategies/scorer/entropy_scorer.py
+++ src/representative_sampler/core/strategies/scorer/entropy_scorer.py
@@ -51,7 +51,8 @@ class FCMEntropyScorer(BaseScorer):
     status = "experimental"
     
     def __init__(self, n_clusters,
-                 m=4, error=0.005, maxiter=1000,
+                 m=4, error=0.005, 
+                 maxiter=1000,
                  init=None,
                  *args, **kwargs
                  ):
@@ -59,18 +60,55 @@ class FCMEntropyScorer(BaseScorer):
         self.m = m
         self.error = error
         self.maxiter = maxiter
+        self.init = init
     
-    def score(self, embedding_obj: EmbeddingResult, **kwargs) -> ScoreCollection:
-        pass
+    def score(self, embedding_obj: EmbeddingResult, **kwargs) -> ScoreCollection:       
+            
+        self.score_collection = self.compute_fuzzy_cmeans_entropy(embedding_obj=embedding_obj)
+        return self.score_collection
     
-    def compute_fuzzy_cmeans_entropy(self, embedding_obj: EmbeddingResult):
+    def compute_fuzzy_cmeans_entropy(self, embedding_obj: EmbeddingResult, *args, **kwargs) -> ScoreCollection:
+        if "m" in kwargs:
+            m = kwargs.get("m")
+        else:
+            m = self.m
+        if "error" in kwargs:
+            error = kwargs.get("error")
+        else:
+            error = self.error
+        if "maxiter" in kwargs:
+            maxiter = kwargs.get("maxiter")
+        else:
+            maxiter = self.maxiter
+        if "init" in kwargs:
+            init = kwargs.get("init")
+        else:
+            init = self.init
+        
+        if "n_clusters" in kwargs:
+            n_clusters = kwargs.get("n_clusters")
+        else:
+            n_clusters = self.n_clusters
+            
         embeddings = embedding_obj.embedding
         embedding_names = embedding_obj.embedding_name
         
-        fuzz.cluster.cmeans(embeddings.T, c=self.n_centers, m=self.m,
-                            error=self.error, maxiter=self.maxiter, init=self.error
-                            )
-                        
-    
+        cntr, u, _, _, _, _, _ = fuzz.cluster.cmeans(embeddings.T, 
+                                                     c=n_clusters, 
+                                                     m=m,
+                                                    error=error, 
+                                                    maxiter=maxiter, 
+                                                    init=init
+                                                    )
+        sample_entropies = np.array([entropy(proba) for proba in u.T])
+        normalized_entropies = sample_entropies / np.log(self.n_clusters)
+
+        entropy_scores = [ScoringResult(object_name=embedding_nm, 
+                                        score=score,
+                                        scorer_name=self.scorer_name
+                                        ) 
+                          for embedding_nm, score, in zip(embedding_names, normalized_entropies)
+                          ]
+        return ScoreCollection(entropy_scores)
     
     
