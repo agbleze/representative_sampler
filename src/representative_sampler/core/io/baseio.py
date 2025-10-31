99a3b8232587a1795b4cd1c32a91eb2947749903
linus <lagbleze@st.ug.edu.gh>
2025-10-31 23:45:32 +0100

    feat: add baseio

--- src/representative_sampler/core/io/baseio.py
+++ src/representative_sampler/core/io/baseio.py
@@ -8,4 +8,15 @@ class BaseImporter(object):
     
     def __init_subclass__(self, cls):
         super().__init_subclass__()
+        
+        for nm in cls.required:
+            if nm not in cls.__dict__:
+                raise NotImplementedError(f"{cls.__name__} requires definining a '{nm}' class attribute because it is a Subclass of BaseImporter.")
+        if cls.status not in cls.valid_status_values:
+            raise ValueError(f"{cls.__name__} has invalid status '{cls.status}'. Valid values are {cls.valid_status_values}.")
+        
+    @abstractmethod
+    def import_data(self, *args, **kwargs):
+        raise NotImplementedError("Subclasses must implement the import_data method.")
+            
     
