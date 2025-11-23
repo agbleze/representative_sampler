f492fb38032079e33e0c5506486494fe820fcf56
linus <lagbleze@st.ug.edu.gh>
2025-11-23 22:24:21 +0100

    fix: resolved import path

---
+++ src/representative_sampler/core/scorer/feature_entropy_scorer.py
@@ -0,0 +1,39 @@
+from .base_scorer import BaseScorer
+from representative_sampler.core.entities import EmbeddingResult, ScoringResult
+from ..object_collections import ScoreCollection
+from scipy.stats import entropy
+import numpy as np
+
+
+class FeatureEntropyScorer(BaseScorer):
+    scorer_name = "feature_entropy_scorer"
+    status = "experimental"
+    
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def score(self, embedding_obj: EmbeddingResult, **kwargs) -> ScoreCollection:
+        self.score_collection = self.compute_image_feature_entropy(embedding_obj=embedding_obj,
+                                                                   **kwargs
+                                                                   )
+        return self.score_collection
+    
+    def compute_image_feature_entropy(self, embedding_obj: EmbeddingResult,
+                                      **kwargs
+                                      ) -> ScoreCollection:
+        embedding = embedding_obj.embedding
+        embedding_names = embedding_obj.embedding_name
+        if embedding.size == 1:
+            image_entropy = entropy(embedding)
+        else:
+            image_entropy = np.array([entropy(emb) for emb in embedding])
+            
+        image_entropy_scores = [ScoringResult(object_name=embedding_nm, 
+                                            score=score,
+                                            scorer_name=self.scorer_name
+                                            ) 
+                                for embedding_nm, score, in zip(embedding_names, image_entropy)
+                                ]
+        return ScoreCollection(image_entropy_scores)
+        
+
